7fa9628f855b550d3f6e5d9423793420cb0f29c3
Drew Lemmy <drew@lemmmy.pw>
2020-12-13 21:28:41 +0000

    Add initial recurring tests

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
+import { DateTime, Zone } from "luxon";
+
 export interface RecurringDate {
   type: "recurring";
   cron: string;
@@ -13,7 +15,30 @@ export interface RelativeDate {
   date: Date;
 }
 
-export default function remindate(input: string): RecurringDate | AbsoluteDate | RelativeDate {
+export interface RemindateOptions {
+  /** Used to pre-fill blank date fields. Defaults to today at 9 AM. */
+  referenceDate?: Date | DateTime;
+
+  /** The date to calculate relative dates from, defaults to now. */
+  currentDate?: Date;
+
+  /** IANA timezone offset for absolute calculations. */
+  timezone?: string | Zone;
+}
+
+export default function remindate(input: string, options?: RemindateOptions): RecurringDate | AbsoluteDate | RelativeDate {
+  // Default options
+  options = Object.assign({}, {
+    currentDate: new Date()
+  }, options);
+
+  // Reference date has to be applied after the timezone
+  if (!options.referenceDate) {
+    options.referenceDate = DateTime.fromObject({ hour: 9, zone: options.timezone });
+  } else if (options.referenceDate instanceof Date) {
+    options.referenceDate = DateTime.fromJSDate(options.referenceDate);
+  }
+
   return {
     type: "recurring",
     cron: "* * * * *" 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,1 +1,47 @@
 import remindate from "../lib";
+
+describe("recurring dates", () => {
+  test("every day", () => {
+    expect(remindate("every day")).toHaveProperty("cron", "0 9 * * *");
+    expect(remindate("every days")).toHaveProperty("cron", "0 9 * * *");
+    expect(remindate("every 2 days")).toHaveProperty("cron", "0 9 */2 * *");
+    expect(remindate("every 3 days")).toHaveProperty("cron", "0 9 */3 * *");
+    expect(remindate("every 30 days")).toHaveProperty("cron", "0 9 */30 * *");
+    expect(remindate("every day at 10 pm")).toHaveProperty("cron", "0 22 * * *");
+    expect(remindate("every day at 10:30 pm")).toHaveProperty("cron", "30 22 * * *");
+
+    // case insensitivity check
+    expect(remindate("every DAY")).toHaveProperty("cron", "0 9 * * *");
+  });
+
+  test("every month", () => {
+    expect(remindate("every month")).toHaveProperty("cron", "0 9 1 * *");
+    expect(remindate("every 2 months")).toHaveProperty("cron", "0 9 1 */2 *");
+    expect(remindate("every 3 months")).toHaveProperty("cron", "0 9 1 */3 *");
+  });
+
+  test("every week", () => {
+    expect(remindate("every week")).toHaveProperty("cron", "0 9 * * 1");
+    expect(remindate("every weekday")).toHaveProperty("cron", "0 9 * * 1-5");
+    expect(remindate("on weekdays")).toHaveProperty("cron", "0 9 * * 1-5");
+    expect(remindate("every weekend")).toHaveProperty("cron", "0 9 * * 6");
+    expect(remindate("on weekends")).toHaveProperty("cron", "0 9 * * 6");
+
+    expect(remindate("every sunday")).toHaveProperty("cron", "0 9 * * 0");
+    expect(remindate("every monday")).toHaveProperty("cron", "0 9 * * 1");
+    expect(remindate("every tuesday")).toHaveProperty("cron", "0 9 * * 2");
+    expect(remindate("every wednesday")).toHaveProperty("cron", "0 9 * * 3");
+    expect(remindate("every thursday")).toHaveProperty("cron", "0 9 * * 4");
+    expect(remindate("every friday")).toHaveProperty("cron", "0 9 * * 5");
+    expect(remindate("every saturday")).toHaveProperty("cron", "0 9 * * 6");
+
+    expect(remindate("on sundays")).toHaveProperty("cron", "0 9 * * 0");
+    expect(remindate("on mondays")).toHaveProperty("cron", "0 9 * * 1");
+    expect(remindate("on tuesdays")).toHaveProperty("cron", "0 9 * * 2");
+    expect(remindate("on wednesdays")).toHaveProperty("cron", "0 9 * * 3");
+    expect(remindate("on thursdays")).toHaveProperty("cron", "0 9 * * 4");
+    expect(remindate("on fridays")).toHaveProperty("cron", "0 9 * * 5");
+    expect(remindate("on saturdays")).toHaveProperty("cron", "0 9 * * 6");
+    expect(remindate("on saturdays")).toHaveProperty("cron", "0 9 * * 6");
+  });
+});
